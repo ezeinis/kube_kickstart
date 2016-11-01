0534f89193c8adb5b030742a88ffdf7350cf139e
ezeinis <e.zeinis@darkpony.com>
2016-11-01 16:59:25 +0200

    edited mixins,added variabls scss,added css and js public files

--- resources/views/website_theme/layouts/main_layout.blade.php
+++ resources/views/website_theme/layouts/main_layout.blade.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" type="text/css" href="/css/kube.min.css">
     <link rel="stylesheet" type="text/css" href="/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="/css/main.css">
+    <script type="text/javascript" src="/js/jquery-3.1.1.min.js"></script>
 
     @yield('header')
 </head>
@@ -17,7 +18,6 @@
         @include('website_theme.includes.footer')
     </div>
 
-    <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
     <script type="text/javascript" src="/js/kube.min.js"></script>
     @yield('js')
 </body>
